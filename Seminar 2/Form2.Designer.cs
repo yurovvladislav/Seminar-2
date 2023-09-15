479d5bbff3e078d11a78d4b052bc21c7b5cb809c
yurovvladislav <yuvk.21@uni-dubna.com>
2023-09-15 13:46:10 +0300

    Изменение

--- Seminar 2/Form2.Designer.cs
+++ Seminar 2/Form2.Designer.cs
@@ -1,14 +1,14 @@
 ﻿namespace Seminar_2
 {
-    partial class Form1
+    partial class Form2
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,105 +23,91 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.button2.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(230, 297);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(157, 42);
-            this.button2.TabIndex = 24;
-            this.button2.Text = "Регистрация";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(203, 112);
+            this.label2.Location = new System.Drawing.Point(134, 65);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(72, 28);
-            this.label2.TabIndex = 22;
+            this.label2.TabIndex = 16;
             this.label2.Text = "Логин";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(190, 161);
+            this.label3.Location = new System.Drawing.Point(121, 114);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(85, 28);
-            this.label3.TabIndex = 23;
+            this.label3.TabIndex = 17;
             this.label3.Text = "Пароль";
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
-            this.button1.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(249, 222);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 42);
-            this.button1.TabIndex = 21;
-            this.button1.Text = "Войти";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(281, 161);
+            this.textBox2.Location = new System.Drawing.Point(212, 114);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(125, 27);
-            this.textBox2.TabIndex = 20;
+            this.textBox2.TabIndex = 14;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(281, 113);
+            this.textBox1.Location = new System.Drawing.Point(212, 66);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(125, 27);
-            this.textBox1.TabIndex = 19;
+            this.textBox1.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(160, 205);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 42);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Регистрация";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(190, 378);
+            this.label1.Location = new System.Drawing.Point(0, 291);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(261, 40);
-            this.label1.TabIndex = 25;
-            this.label1.Text = "ВХОД СОВЕРШЁН!";
+            this.label1.Size = new System.Drawing.Size(504, 40);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "РЕГИСТРАЦИЯ ПРОШЛА УСПЕШНО!\r\n";
             this.label1.Visible = false;
             // 
-            // Form1
+            // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(597, 450);
+            this.ClientSize = new System.Drawing.Size(495, 450);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form2_FormClosed);
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -129,12 +115,11 @@
 
         #endregion
 
-        private Button button2;
         private Label label2;
         private Label label3;
-        private Button button1;
         private TextBox textBox2;
         private TextBox textBox1;
+        private Button button2;
         private Label label1;
     }
 }
