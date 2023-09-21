0d38777307a82a24c34166da4729dd9bcd326db1
reechrd17@gmail.com <reechrd17@gmail.com>
2023-09-21 18:30:21 +0300

    add writer

---
+++ Seminar 2/Writer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32.SafeHandles;
+
+namespace Seminar_2
+{
+    internal class Writer
+    {
+        static void Write(string login, string password, string adress) 
+        {
+            StreamWriter sw=new StreamWriter(adress);
+            sw.Write(login+" "+password);
+            sw.Close();
+        }
+    }
+}
