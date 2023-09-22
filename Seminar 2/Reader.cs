3d566b5e494b922c4dd08ac06cc2abd9081c851c
TimDevIt <delootos1@mail.ru>
2023-09-22 13:39:10 +0300

    Добавил класс Reader с методом проверки пользователя в базе данных

---
+++ Seminar 2/Reader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class Reader
+{
+    bool IsUserExists(string path, string login, string password, string adress) {
+        string[] lines = File.ReadAllLines("test.txt");
+        foreach (string s in lines)
+        {
+            string[] data = s.Split(' ');
+            if (data.Length() == 3 && data[0] == login && data[1] == password && data[2] == adress)
+                return true;
+        }
+        return false;
+    }   
+}
