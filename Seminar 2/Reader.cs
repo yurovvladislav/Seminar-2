4c0ebdc37c1439b0035137e6100bd0c658d82f52
tkukushina00@mail.ru <tkukushina00@mail.ru>
2023-10-27 12:54:26 +0300

    Reader.cs : update class -> static class

--- Seminar 2/Reader.cs
+++ Seminar 2/Reader.cs
@@ -2,12 +2,12 @@
 
 public static class Reader
 {
-    bool IsUserExists(string path, string login, string password, string adress) {
+    public static bool IsUserExists(string path, string login, string password) {
         string[] lines = File.ReadAllLines("test.txt");
         foreach (string s in lines)
         {
             string[] data = s.Split(' ');
-            if (data.Length() == 3 && data[0] == login && data[1] == password && data[2] == adress)
+            if (data.Length == 2 && data[0] == login && data[1] == password)
                 return true;
         }
         return false;
